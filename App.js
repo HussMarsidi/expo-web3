87a300e464b859f150bf5b3ba110a63dbe42fee7
Huss <hussaini.marsidi@mili.eu>
2023-03-07 00:24:43 +0800

    update button

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const isLoggedIn = false;
+
+  function userLoggedView() {
+    return (
+      <>
+        <Button title="Logout" />
+
+        <StatusBar style="auto" />
+      </>
+    );
+  }
+
+  function userNotLoggedView() {
+    return (
+      <>
+        <Button title="Sign in" />
+        <View style={{ height: 10 }} />
+        <Button title="Sign up" />
+        <StatusBar style="auto" />
+      </>
+    );
+  }
+
+  return <View style={styles.container}>{isLoggedIn ? userLoggedView() : userNotLoggedView()}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
